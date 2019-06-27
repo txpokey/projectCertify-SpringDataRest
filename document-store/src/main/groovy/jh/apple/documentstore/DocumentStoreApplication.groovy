45bb1faf4118ce52f6c8b3cad86c5849bc86fe48
Bob Makowski <tritonpi-git0@yahoo.com>
2019-06-26 20:05:42 -0500

    -:-: ad initio from spring initializer

---
+++ document-store/src/main/groovy/jh/apple/documentstore/DocumentStoreApplication.groovy
@@ -0,0 +1,13 @@
+package jh.apple.documentstore
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DocumentStoreApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DocumentStoreApplication, args)
+	}
+
+}
